3836067ae51327dac9adaaa35246b30a622860ad
Dmitriy S <andry_sila@mail.ru>
2025-04-07 01:59:26 +0500

    first push

---
+++ src/types/index.ts
@@ -0,0 +1,48 @@
+/**
+ * Категории товаров в магазине
+ */
+
+export type TCategory = 'софт-скил' | 'другое' | 'дополнительное' | 'кнопка' | 'хард-скил';
+
+/**
+ * Информация о товаре
+ */
+
+export interface IProduct {
+    id: number;
+    title: string;
+    description?: string;
+    category: TCategory;
+    image: string;
+    price: number | null; 
+}
+ 
+/**
+ * Данные формы оформления заказа
+ */
+
+export interface IOrderForm {
+    address?: string;
+    payment?: string;
+    phone?: string;
+    email?: string;
+    total?: number;
+    }
+
+/**
+ * Полный заказ с товарами
+ */
+
+    export interface IOrder extends IOrderForm {
+        products: string[];
+    }
+
+/**
+ * Ответ при успешном оформлении заказа
+ */
+
+    export interface IOrderResult {
+        success: boolean;
+        id:string; 
+        total: number;
+    }
